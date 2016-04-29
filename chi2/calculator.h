d960b97ecd7f669de8b6ce95f9131b4fa445c760
theodor-garbuzov <fedor.garbuzov@gmail.com>
2016-04-29 16:06:24 +0300

    Согласованный интерфейс (доделка)

--- chi2/calculator.h
+++ chi2/calculator.h
@@ -21,6 +21,7 @@ public:
 
  private:
     // работа с кэшем
+
     DirectCalculation* calc;
 
 };
--- chi2/dbinteraсtion.h
+++ chi2/dbinteraсtion.h
@@ -17,7 +17,7 @@ typedef struct {
 typedef QPair<double, double> Point; // chi2, value
 typedef QVector<Point> PointVector;
 
-class DBInteraction: public QWidget
+class DBInteraction: public QObject
 {
     Q_OBJECT
 public:
@@ -28,23 +28,6 @@ public:
         returns true if connection has been set, fasle if not
     */
     bool addConnection(QString host, QString dbname, QString user, QString password);
-
-    /** Get information about each sample in data base
-    */
-    SamplesInfo getSamplesInfo();
-
-    /** Get sample from data base
-      */
-    Sample getSample(QString sampleName);
-
-    /** Get already calculated chi2 statistics
-        for samples with the same degrees of freedom
-    */
-    PointVector getCachedPoints(size_t degreesOfFreedom);
-
-    /** Cache calculated point (save it in database)
-      */
-    void cachePoint(Point);
 
     /** Close current connection
       */
